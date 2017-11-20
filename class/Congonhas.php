4c59aa967335f6716ec246efdb40bf94b6d6dd68
Marcos Santiago <marcos14952010@hotmail.com>
2017-11-20 18:42:47 -0200

    added new methods for api

--- class/Congonhas.php
+++ class/Congonhas.php
@@ -48,7 +48,60 @@ class Congonhas
         return $array;
     }
 
+    /**
+     * @param $login
+     * @param $password
+     * @param $client
+     * @param $document
+     * @return mixed
+     */
+    public function getCadastralSituation($login, $password, $client, $document)
+    {
+        $token = $this->getToken($login, $password, $client);
+        $document = $this->client->request('GET', 'http://wsnv.novavidati.com.br/WSLocalizador.asmx/SituacaoCadastralTK', [
+            'query'=> ['documento'=> $document, 'token' => $token]
+        ]);
 
+        $array = xmlConverter(replaceWords($document->getBody()));
 
+        return $array;
+    }
 
+    /**
+     * @param $login
+     * @param $password
+     * @param $client
+     * @param $document
+     * @return mixed
+     */
+    public function getSociodemographicProfile($login, $password, $client, $document)
+    {
+        $token = $this->getToken($login, $password, $client);
+        $document = $this->client->request('GET', 'http://wsnv.novavidati.com.br/WSLocalizador.asmx/PerfilSocioDemograficoTK', [
+            'query'=> ['documento'=> $document, 'token' => $token]
+        ]);
+
+        $array = xmlConverter(replaceWords($document->getBody()));
+
+        return $array;
+    }
+
+    /**
+     * @param $login
+     * @param $password
+     * @param $client
+     * @param $document
+     * @return mixed
+     */
+    public function getAffinity($login, $password, $client, $document)
+    {
+        $token = $this->getToken($login, $password, $client);
+        $document = $this->client->request('GET', 'http://wsnv.novavidati.com.br/WSLocalizador.asmx/AfinidadeTK', [
+            'query'=> ['documento'=> $document, 'token' => $token]
+        ]);
+
+        $array = xmlConverter(replaceWords($document->getBody()));
+
+        return $array;
+    }
 }
